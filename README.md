fd9e957a78fead995b56ccaab7fa961c62b16933
wurong <1159228556@qq.com>
2026-03-12 12:54:59 +0800

    add AgentGym-RL

--- README.md
+++
@@ -1,5 +0,0 @@
-当前仓库含有子模块，克隆需使用
-
-```bash
-git clone --recurse-submodules
-```
---
+++ llm_client_demo.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+
+
+def LLM_demo(human_input, model_name="gpt-4o-mini", max_tokens=4096, temperature=0.7, top_p=0.9):
+
+    client = OpenAI(
+    api_key="sk-5QyBNRgeFFiX6sY1aooYjvtygjNelFW87I6ziXkE6mP6tVeH", 
+    base_url="http://10.140.37.34:8000/v1/")
+
+    try:
+        # 调用 Chat Completion API 并设置参数
+        completion = client.chat.completions.create(
+            model = model_name,
+            messages=[
+                {'role': 'system', 'content': 'You are a helpful assistant.'},
+                {'role': 'user', 'content': human_input}],
+            max_tokens=max_tokens,
+            )
+        return completion.model_dump()["choices"][0]["message"]["content"]
+        
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+if __name__ == '__main__':
+    model_name = "Qwen2.5-0.5b"
+    response1 = LLM_demo("你好，你是谁？", model_name)
+    print(response1)
+    response2 = LLM_demo("Who are you?", model_name)
+    print(response2)
